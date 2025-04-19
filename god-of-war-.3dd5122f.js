5386ca6340abae015a96aa475ff2b337e5e968f6
LavrovMykola <147297868+LavrovMykola@users.noreply.github.com>
2025-04-19 19:25:18 +0000

    Deploying to gh-pages from @ dima0073231/god-of-war-@34674d10ea189a02092d559cf5c353bd6cd0c210 🚀

---
+++ god-of-war-.3dd5122f.js
@@ -0,0 +1,2 @@
+document.getElementById("playButton").addEventListener("click",function(){var e=document.getElementById("gameTrailer");e.style.display="block",e.contentWindow.postMessage(JSON.stringify({event:"command",func:"playVideo",args:[]}),"*")});
+//# sourceMappingURL=god-of-war-.3dd5122f.js.map
